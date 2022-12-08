2d0066f3a3525cace1376e8f3fbc6701bab31ea0
Matheus <matheusferreirapimenta1@gmail.com>
2022-12-08 14:27:51 -0300

    OneOnOne

--- assets/components/HelloWorld.tsx
+++ assets/components/HelloWorld.tsx
@@ -3,6 +3,9 @@ import * as S from "../styles/Tittle";
 
 export default function AppHello() {
     return(
-        <S.Tilte>Olá web!</S.Tilte>
+        <>
+            <S.Tilte>Projeto One On One</S.Tilte>
+        </>
+
     );
 }
