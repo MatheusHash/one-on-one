d7eb9761c6bbe5056a0358396f854f20ff51429e
Matheus <matheusferreirapimenta1@gmail.com>
2022-12-09 09:49:27 -0300

    Include FontAwesome Packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-
+import { faGhost  } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function Home() {
   return (
     <>
+        <FontAwesomeIcon icon={faGhost} fontSize='200px' />
     </>
   )
 }
